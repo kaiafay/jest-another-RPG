80d93bc7a05bf471f7ab1cfba4a29b8e5bdee11c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-29 12:57:27 -0700

    create failing test for player's attack value

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -60,3 +60,11 @@ test("subtracts from player's health", () => {
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
 });
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
