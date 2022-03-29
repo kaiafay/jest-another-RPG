39a4da4a37140edd5eac06e338c793bcd0115ac2
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-28 18:31:08 -0700

    write failing test for player's stats

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
 const Player = require('../lib/Player');
+const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 // replaces constructor's implementation with faked data
 jest.mock('../lib/Potion');
@@ -13,4 +14,13 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stat's as an object", () => {
+    const Player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
 });
