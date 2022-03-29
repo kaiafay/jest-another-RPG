bd99bd5cf72fa4d689f749dea323043e559a7da1
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-29 13:02:42 -0700

    create failing test for player's inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -68,3 +68,11 @@ test("gets player's attack value", () => {
     expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
     expect(player.getAttackValue()).toBeLessThanOrEqual(15);
 });
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+})
